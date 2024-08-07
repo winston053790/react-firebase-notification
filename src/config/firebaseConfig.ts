31e99238b14ed01e9a1c364c55691d11b3916f3a
winston053790 <winston052790@gmail.com>
2024-08-08 04:32:15 +0800

    add env

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -3,14 +3,14 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD4_3hwnUlrNWs8f2C5cotDpT5giFfR2u0',
-  authDomain: 'react-notification-51dd5.firebaseapp.com',
-  databaseURL: 'https://react-notification-51dd5-default-rtdb.firebaseio.com',
-  projectId: 'react-notification-51dd5',
-  storageBucket: 'react-notification-51dd5.appspot.com',
-  messagingSenderId: '165989974827',
-  appId: '1:165989974827:web:c43039f890add3dc8abb6e',
-  measurementId: 'G-HQYSZ27KGL'
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 }
 
 const app = initializeApp(firebaseConfig)
